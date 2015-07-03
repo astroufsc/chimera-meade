58ed153dfa9a6e8df3bcc9a003d8814c90c12335
William Schoenell <wschoenell@gmail.com>
2015-07-03 15:32:39 +0200

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='chimera-serial',
+    name='chimera-meade',
     version='0.0.1',
-    packages=['chimera_serial', 'chimera_serial.instruments'],
+    packages=['chimera_meade', 'chimera_meade.instruments'],
     scripts=[],
-    url='http://github.com/astroufsc/chimera-serial',
+    url='http://github.com/astroufsc/chimera-meade',
     license='GPL v2',
     author='William Schoenell',
     author_email='william@iaa.es',
     install_requires=['pyserial'],
-    description='Chimera plugin for serial devices'
+    description='Chimera plugin for MEADE telescopes'
 )
